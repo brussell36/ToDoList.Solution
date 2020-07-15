8e8bcdfc275bf4da8c78083dad167e31718fcb9e
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-15 12:41:02 -0600

    add program.cs functionality to create item and list instances.
    Co-Authored-By: Ben Russell benjaminrussell36@gmail.com
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,14 +1,35 @@
-// using ToDoList.Models;
-// using System;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
 
-// namespace ToDoList
-// {
-//  class Program
-//  {
+namespace ToDoList
+{
+  class Program
+  {
 
-//    public static void Main()
-//    {
-
-//    }
-//  }
-// }
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to the To Do List!");
+      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+      string input = Console.ReadLine();
+      
+        if (input == "Add")
+        {
+          Console.WriteLine("Please enter the description for the new item.");
+          string description = Console.ReadLine();
+          Item newItem = new Item(description);
+          List<Item> ItemList = new List<Item>{};  
+          string addItem = newItem.Description;
+          Console.WriteLine(addItem + " has been added to your list. Would you like to add an item to you list or view you list?(Add/View)");
+        }
+        else if (input == "View")
+        {
+          List<Item> result = Item.GetAll();
+          Console.WriteLine(result);
+        }
+        else
+        {
+          Main();
+        }    }
+  }
+}
